cc131d259bfd58199fa4290a3a862b3b80a9be04
Voravit Tanyingyong <voravit@kth.se>
2019-05-29 12:54:44 +0200

    Scripts to run experiments: autonomous and uncoordinated decision

---
+++ examples/ipv6/br_fixed/loop_at_rx.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+OUTPUT=16
+if [ -z "$1" ]; then
+	SEED=123456
+else
+	SEED=$1
+fi
+if [ -z "$2" ]; then
+	VALUE=128
+else
+	VALUE=$2
+fi
+
+
+echo "SEED: $SEED VALUE: $VALUE"
+
+BASE_TEST="/home/voravit/repo/current/main/contiki/examples/ipv6/br_fixed"
+cd $BASE_TEST
+
+START=`date "+%Y%m%d"`
+echo "mkdir ${START}_${SEED}_at_rx"
+mkdir ${START}_${SEED}_at_rx
+
+RATE=(2 3 4 5 6)
+echo "### START TEST 16 NODES"
+for r in ${RATE[@]}
+do
+	echo "# rate: $r"
+	./at_rx.sh $r $SEED $VALUE
+done
+
+mv at_rx* ${START}_${SEED}_at_rx
