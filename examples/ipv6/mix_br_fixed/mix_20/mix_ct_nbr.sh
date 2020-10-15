066b846b4facd2e97b323d4145d2c4c479d137c3
Voravit Tanyingyong <voravit@kth.se>
2020-10-15 16:34:52 +0200

    mixed traffic load experiment

---
+++ examples/ipv6/mix_br_fixed/mix_20/mix_ct_nbr.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+SINKS=9
+CS=$((${SINKS}-1))
+SEED=$1
+VALUE=$2
+THRESHOLD=$3
+OUTPUT=16
+SERVER="NOREPLY"
+# PC
+#BASE_TEST="/home/voravit/repo/contiki/examples/ipv6/test_collect"
+#BASE_COOJA="/home/voravit/repo/contiki/tools/cooja"
+# laptop
+BASE_TEST="/home/voravit/repo/current/main/contiki/examples/ipv6/mix_br_fixed"
+BASE_COOJA="/home/voravit/repo/current/main/contiki/tools/cooja"
+LOGFILE=${BASE_TEST}/${SEED}.log
+cd $BASE_TEST
+
+touch $LOGFILE
+
+CTR=0
+	cd $BASE_TEST
+	#rm *wismote
+	#make TARGET=wismote clean
+
+	#cp grid_64_s${SINKS}.csc run.csc
+	cp rand_cs${CS}.csc run.csc
+	sed -i "s/<randomseed>123456/<randomseed>${SEED}/" run.csc
+	#sed -i "s/MAX_RX_TRAFFIC 3000/MAX_RX_TRAFFIC ${RX_TRAFFIC}/" coordinator.c
+        #/usr/bin/gcc -o coordinator coordinator.c
+	sed -i "s/<success_ratio_rx>1\.0/<success_ratio_rx>0\.2/" run.csc
+        cp -p project-conf.h.rand project-conf.h
+	sed -i "s/SINK_ADDITION 1/SINK_ADDITION 3/" project-conf.h
+	cp -p coordinator.c.clean coordinator.c
+	sed -i "s/NBR_TH_ACT 50/NBR_TH_ACT ${VALUE}/" coordinator.c
+	sed -i "s/NBR_TH_DEACT 15/NBR_TH_DEACT 20/" coordinator.c
+        sed -i "s/RX_TH_DEACT 15/RX_TH_DEACT 20/" coordinator.c
+	sed -i "s/RX_METRIC 1/RX_METRIC 0/" coordinator.c
+	sed -i "s/THRESHOLD_HIT 3/THRESHOLD_HIT ${THRESHOLD}/" coordinator.c
+	/usr/bin/gcc -o coordinator coordinator.c
+
+        cd ../mix_br_sensor
+        cp -p project-conf.h.rand project-conf.h
+	sed -i "s/SINK_ADDITION 1/SINK_ADDITION 3/" project-conf.h
+        cd ../mix_sender_cbr
+        cp -p project-conf.h.rand project-conf.h
+        cd ../mix_sender_poisson
+        cp -p project-conf.h.rand project-conf.h
+        cd ../mix_sender_vary
+        cp -p project-conf.h.rand project-conf.h
+        cd ../mix_br_fixed
+
+	cp -a /home/voravit/repo/current/main/contiki/core/net/rpl/rpl-metric-timer.c.at.rank /home/voravit/repo/current/main/contiki/core/net/rpl/rpl-metric-timer.c
+#        sed -i "s/ARR_RXH_TH 50/ARR_RXH_TH ${VALUE}/" /home/voravit/repo/current/main/contiki/core/net/rpl/rpl-metric-timer.c
+	sed -i "s/THRESHOLD_HIT 3/THRESHOLD_HIT ${THRESHOLD}/" /home/voravit/repo/current/main/contiki/core/net/rpl/rpl-metric-timer.c
+
+	cd $BASE_COOJA/build
+	FILE=${BASE_TEST}/run.csc
+
+	echo ">>>>> Running SEED:$SEED" >> $LOGFILE
+	echo "START:" `date "+%Y-%m-%d %H:%M:%S %N"` >> $LOGFILE
+	echo "START:"
+	#sudo ${BASE_TEST}/setup_route.sh 2 & >> $LOGFILE 2>&1
+	sudo ${BASE_TEST}/setup_route.sh $SINKS $SERVER & 
+	java -mx1024m -jar ${BASE_COOJA}/dist/cooja.jar -nogui=$FILE
+	ERR=`grep "FATAL" COOJA.log | wc -l`
+	FINISHED=`grep "finished" COOJA.log | wc -l`
+	while [ $ERR -ge 1 ] && [ $FINISHED -ne 1 ];
+	do
+	  CTR=$((CTR+1))
+	  echo "FATAL ERROR! $CTR" >> $LOGFILE
+	  cd $BASE_COOJA/build
+	  mkdir fatal${CTR}
+  	  mv COOJA* fatal${CTR}
+  	  mv log*txt radiolog*pcap fatal${CTR}
+	  mv fatal${CTR} ${BASE_TEST}/${SEED}/.
+
+	cd $BASE_TEST
+	rm *wismote
+	make TARGET=wismote clean
+
+	  cd $BASE_COOJA/build
+
+	  echo "RERUN:$CTR SEED:$SEED GRID:$g OF:$f MODE:$l" >> $LOGFILE
+	  java -mx1024m -jar ${BASE_COOJA}/dist/cooja.jar -nogui=$FILE
+	  ERR=`grep "FATAL" COOJA.log | wc -l`
+	  FINISHED=`grep "finished" COOJA.log | wc -l`
+	done
+
+	echo "STOP:" `date "+%Y-%m-%d %H:%M:%S %N"` >> $LOGFILE
+	sleep 2
+	mkdir ct_nbr_${VALUE}
+	mv COOJA* log*txt ct_nbr_${VALUE}
+	mv *pcap ct_nbr_${VALUE}
+	mv tun*txt ct_nbr_${VALUE}
+	mv nohup.out ct_nbr_${VALUE}
+	mv route.out ct_nbr_${VALUE}
+	mv ct_nbr_${VALUE} ${BASE_TEST}/.
+	cd $BASE_TEST
+	mv $LOGFILE ct_nbr_${VALUE}/.
+	mv udp.out ct_nbr_${VALUE}/.
+	mv coordinator.out ct_nbr_${VALUE}/.
---
+++ examples/ipv6/mix_br_fixed/mix_loop6/lp_mix_s4.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+OUTPUT=16
+if [ -z "$1" ]; then
+	SEED=123456
+else
+	SEED=$1
+fi
+if [ -z "$2" ]; then
+	VALUE=128
+else
+	VALUE=$2
+fi
+if [ -z "$3" ]; then
+	THRESHOLD=1
+else
+	THRESHOLD=$3
+fi
+
+
+
+BASE_TEST="/home/voravit/repo/current/main/contiki/examples/ipv6/mix_br_fixed"
+cd $BASE_TEST
+
+START=`date "+%Y%m%d"`
+echo "mkdir ${START}_mix"
+mkdir ${START}_mix
+
+VALUE=60
+THRESHOLD=1
+SEED=(600000 700000 800000 900000 1000000)
+#SEED=(600000 700000 800000 900000 1000000)
+#SEED=(100000 200000 300000 400000 500000 600000 700000 800000 900000 1000000)
+for s in ${SEED[@]}
+do
+	echo "SEED: $s VALUE: $VALUE THRESHOLD: $THRESHOLD"
+	./mix_s4.sh $s
+	mkdir $s
+	mv s_4 $s
+	mv $s ${START}_mix
+done
+
